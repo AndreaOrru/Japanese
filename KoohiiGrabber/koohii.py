d7486768bed0acb20eb0964965d96f6a91385420
Andrea Orru <andrew.iwakura@gmail.com>
2015-11-16 04:35:31 +0000

    Dirtified.

--- KoohiiGrabber/koohii.py
+++ KoohiiGrabber/koohii.py
@@ -7,45 +7,44 @@ from getpass import getpass
 from time import sleep
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('koohii', description='Grab the best stories from Kanji Koohii.')
-    required = parser.add_argument_group('required arguments')
-    required.add_argument('-i', metavar='INPUT',  required=True, help='TSV file containing the kanji info')
-    required.add_argument('-o', metavar='OUTPUT', required=True, help='TSV file where to export the stories')
+parser = argparse.ArgumentParser('koohii', description='Grab the best stories from Kanji Koohii.')
+required = parser.add_argument_group('required arguments')
+required.add_argument('-i', metavar='INPUT',  required=True, help='TSV file containing the kanji info')
+required.add_argument('-o', metavar='OUTPUT', required=True, help='TSV file where to export the stories')
 
-    parser.add_argument('-n', metavar='STORIES', default=5, type=int, help='number of stories per kanji (default 5)')
-    args = parser.parse_args()
+parser.add_argument('-n', metavar='STORIES', default=5, type=int, help='number of stories per kanji (default 5)')
+args = parser.parse_args()
 
-    username =   input('Koohii username: ')
-    password = getpass('Koohii password: ')
+username =   input('Koohii username: ')
+password = getpass('Koohii password: ')
 
-    browser = mechanicalsoup.Browser()
-    login_page = browser.get('http://kanji.koohii.com/login')
-    login_form = login_page.soup.select('form')[0]
-    login_form.select('#username')[0]['value'] = username
-    login_form.select('#password')[0]['value'] = password
-    browser.submit(login_form, login_page.url)
+browser = mechanicalsoup.Browser()
+login_page = browser.get('http://kanji.koohii.com/login')
+login_form = login_page.soup.select('form')[0]
+login_form.select('#username')[0]['value'] = username
+login_form.select('#password')[0]['value'] = password
+browser.submit(login_form, login_page.url)
 
-    rtk    = open(args.i).readlines()
-    output = open(args.o, 'w')
+rtk    = open(args.i).readlines()
+output = open(args.o, 'w')
 
-    for line in rtk:
-        line = line.split('\t')
+for line in rtk:
+    line = line.split('\t')
 
-        kanji   = line[1]
-        number  = line[3]
-        keyword = line[6]
-        print('{:<4}  {}  {}'.format(number, kanji, keyword))
+    kanji   = line[1]
+    number  = line[3]
+    keyword = line[6]
+    print('{:<4}  {}  {}'.format(number, kanji, keyword))
 
-        page = browser.get('http://kanji.koohii.com/study/kanji/{}'.format(kanji))
-        stories = page.soup.select('#sharedstories-fav .story')[:args.n]
-        stories = [''.join(map(str, x.contents)) for x in stories]
-        stories = [re.sub(r'<a href="/study/kanji/(.*?)">(.*?)</a>.*?</span>\)',
-                          r'<a href="http://kanji.koohii.com/study/kanji/\1">\2</a>',
-                          x) for x in stories]
-        stories = (stories + ['']*args.n)[:args.n]
+    page = browser.get('http://kanji.koohii.com/study/kanji/{}'.format(kanji))
+    stories = page.soup.select('#sharedstories-fav .story')[:args.n]
+    stories = [''.join(map(str, x.contents)) for x in stories]
+    stories = [re.sub(r'<a href="/study/kanji/(.*?)">(.*?)</a>.*?</span>\)',
+                      r'<a href="http://kanji.koohii.com/study/kanji/\1">\2</a>',
+                      x) for x in stories]
+    stories = (stories + ['']*args.n)[:args.n]
 
-        output.write('{}\t{}\t{}\t{}\n'.format(number, kanji, keyword, '\t'.join(stories)))
-        sleep(2)
+    output.write('{}\t{}\t{}\t{}\n'.format(number, kanji, keyword, '\t'.join(stories)))
+    sleep(2)
 
 output.close()
